ab88aecc30d192ba2407fae21ddf0f65dc027354
Thomas Eduardo <developer.thomas@outlook.com.br>
2024-11-26 02:32:44 -0300

    Atualizar o banner.js

--- src/script/banner.js
+++ src/script/banner.js
@@ -1,10 +1,19 @@
-// Lista de textos que você deseja exibir automaticamente
 const texts = [
   "Transforme seu Atendimento com CRM e Bots",
   "Aumente suas Vendas com Funis de Vendas Eficientes",
   "Maximize seu Engajamento nas Redes Sociais",
   "Crie Campanhas Publicitárias Personalizadas para Seu Público",
-  "Envolva Seu Público com Mensagens Rápidas e Eficientes"
+  "Envolva Seu Público com Mensagens Rápidas e Eficientes",
+  "Automatize Processos e Ganhe Mais Tempo para o Que Importa",
+  "Converta Mais Leads com Estratégias de Marketing de Conteúdo",
+  "Melhore a Experiência do Cliente com Soluções Inteligentes",
+  "Analise Dados em Tempo Real para Decisões Mais Eficazes",
+  "Otimize Seu Site para Ranquear no Topo das Pesquisas",
+  "Capte Leads de Forma Inteligente e Sustentável",
+  "Aumente a Retenção de Clientes com Ações de Fidelização",
+  "Aproveite ao Máximo o Poder do Marketing Digital",
+  "Revolucione Suas Estratégias de E-commerce Agora Mesmo",
+  "Impacte Seu Público com Comunicação Autêntica e Criativa"
 ];
 
 // Função para trocar o texto
@@ -20,4 +29,4 @@ function updateText() {
 }
 
 // Atualiza o texto a cada 5 segundos (5000 ms)
-setInterval(updateText, 5000);
+setInterval(updateText, 9000);
